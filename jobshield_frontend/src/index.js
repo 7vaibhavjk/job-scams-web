4e97e0c8264790cd555a89e0e97f49cc5118e6c7
gaotong <gt841151806@outlook.com>
2025-10-16 15:47:07 +1100

    Update package

--- jobshield_frontend/src/index.js
+++ jobshield_frontend/src/index.js
@@ -1,10 +1,16 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';  // ← 新增
 import App from './App';
+
+const basename = process.env.REACT_APP_BASENAME || '/'; // ← 新增
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename={basename}>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
